04edc0fad209d79e1d6fbd18e6c626b470fbb592
smoyame <smoyamedina@outlook.com>
2025-04-25 17:44:56 -0400

    Obfuscate upload's filename, store original name; save properly in db

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -5,14 +5,47 @@ export const Media: CollectionConfig = {
   access: {
     read: () => true,
   },
+  hooks: {
+    beforeOperation: [
+      ({ req, operation, context }) => {
+        if ((operation === 'create') && req.file) {
+          context.uploadedFilename = req.file.name
+          const extension = req.file.name.match(/.\w*$/)
+          req.file.name = globalThis.crypto.randomUUID() + extension
+        }
+      }
+    ],
+    afterChange: [
+      async ({ collection, context, doc, req, operation }) => {
+        if (operation === 'create' || operation === 'update') {
+          if (!context.updatePerformed) {
+            context.updatePerformed = true
+            await req.payload.update({
+              req,
+              collection: collection.slug,
+              data: {
+                url: doc.url,
+              },
+              where: {
+                id: {
+                  equals: doc.id
+                },
+              }
+            })
+          }
+        }
+      }
+    ]
+  },
   fields: [
     {
       type: 'text',
       name: 'origFilename',
       label: 'Original Filename',
       admin: {
-        condition: (data) => Boolean(data.origFilename)
-      }
+        condition: (data) => Boolean(data.origFilename),
+        readOnly: true
+      },
     },
     {
       name: 'alt',
