eee5de7bc1ced5d90a1cf30f5ddc340d4d525e13
smoyame <smoyamedina@outlook.com>
2025-04-28 11:52:34 -0400

    Add independent Client collection fields

--- src/collections/micro/Client.ts
+++ src/collections/micro/Client.ts
@@ -14,5 +14,26 @@ export const Client: CollectionConfig = {
 			name: 'name',
 			label: 'Client Name',
 		},
+		{
+			type: 'text',
+			name: 'parentOrg',
+			label: 'Parent Organization'
+		},
+		{
+			type: 'text',
+			name: 'abbr',
+			label: 'Code',
+			maxLength: 6,
+			minLength: 2,
+			admin: {
+				description: `The shortcode used to refer to this client internally.`
+			}
+		},
+		{
+
+			type: 'text',
+			name: 'site',
+			label: 'Website'
+		}
 	]
 }
